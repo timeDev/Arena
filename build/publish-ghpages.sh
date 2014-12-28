f172d097440bc953dd78839d8d9cac939f197a14
timedev <timecraftor@gmail.com>
2014-12-28 02:49:57 +0100

    Do not leak github tokens

--- build/publish-ghpages.sh
+++ build/publish-ghpages.sh
@@ -9,4 +9,4 @@ git add -A
 git config user.name "Travis Build"
 git config user.email "oshomburg@gmail.com"
 git commit -m "push to github pages (auto)" -m "$TRAVIS_COMMIT_MSG"
-git push $REPO_URL gh-pages
+git push -q origin gh-pages
