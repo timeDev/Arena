cc5b6f7b549e3bbc8d170080737acba7e05882b2
Travis Build <oshomburg@gmail.com>
2015-02-08 11:41:58 +0000

    push to github pages (auto)
    Fix tp command

--- game.js
+++ game.js
@@ -561,13 +561,13 @@ exports.firstPersonCam = function (camera) {
 
 exports.commands = {};
 exports.commands.tp = {
-    isCvar:false,
-    name:'tp',
-    ctx:{'cl':commands.contexts.host},
+    isCvar: false,
+    name: 'tp',
+    ctx: {'cl': commands.contexts.host},
     handler: function (args) {
         commands.validate(['number', 'number', 'number'], args);
-        exports.physBody.position.set(args[0], args[1], args[2]);
-        exports.sceneObj.position.set(args[0], args[1], args[2]);
+        exports.physBody.position.set(args[1], args[2], args[3]);
+        exports.sceneObj.position.set(args[1], args[2], args[3]);
     }
 };
 
